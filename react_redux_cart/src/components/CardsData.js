431af44581a47ead6fb6a0b2543937553aeef9be
Akashr18 <sujayakash07@gmail.com>
2022-05-25 20:24:21 +0530

    commit

---
+++ react_redux_cart/src/components/CardsData.js
@@ -0,0 +1,67 @@
+const Cardsdata = [
+    {
+        id: 1,
+        Name: "Weird Fantasy",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK9Rrbo3o4bC58zifBJcnEKhIAVAlZLE-kdQ&usqp=CAU",
+        price: 350,
+        rating: "3.8",
+    },
+    {
+        id: 2,
+        Name: "The Magic Fish",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdZoHfk_QkoOU-tamfyqHajIf98P_1dIWVAQ&usqp=CAU",
+        price: 150,
+        rating: "3.6",   
+    },
+    {
+        id: 3,
+        Name: "Venus",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiFqZPrpPrqb9JZ0MrbkeVmX-JjJR1pQKkvQ&usqp=CAU",
+        price: 60,
+        rating: "3.6",
+    },
+    {
+        id: 4,
+        Name: "Waste Tide",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAI0NmAz_yCmtYF3uyJ-979bBWNOpG7J4fBQ&usqp=CAU",
+        price: 150,
+        rating: "3.5",
+    },
+    {
+        id: 5,
+        Name: "Cats Vs Robots",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRud-ivH6iWZq-xCXOlKvUrNRNw2yyQgDeFLw&usqp=CAU",
+        price: 70,
+        rating: "3.8",
+    },
+    {
+        id: 6,
+        Name: "The Gravity",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwxVTgvzRXO0FonRLZvUsx9iYUpVh1Z8tnLQ&usqp=CAU",
+        price: 250,
+        rating: "4.3",
+    },
+    {
+        id: 7,
+        Name: "The Sound Of Stars",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5nU3BjPnT5aV2kgN3-6Ts9cIoXvUrnkF7og&usqp=CAU",
+        price: 250,
+        rating: "4.8",
+    },
+    {
+        id: 8,
+        Name: "The Hunger Games",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkwMSDXlaNm_kxdC9LRcGKLhMcXByrrBbWQQ&usqp=CAU",
+        price: 90,
+        rating: "4.2",
+    },
+    {
+        id: 9,
+        Name: "Ikigai",
+        imgdata: "https://d2g9wbak88g7ch.cloudfront.net/productimages/mainimages/895/9781786330895.jpg",
+        price: 170,
+        rating: "4.8",
+    },
+];
+
+export default Cardsdata;
